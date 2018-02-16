875943942b7b836675a8b20cf9614dd0294ca467
Lennart Kroneman <a.l.kroneman@gmail.com>
2018-02-16 22:37:12 +0100

    Add aliases and remove ssh key setup for now

--- src/os/set_github_ssh_key.sh
+++ src/os/set_github_ssh_key.sh
@@ -101,26 +101,26 @@ test_ssh_connection() {
 
 main() {
 
-    print_in_purple "\n • Set up GitHub SSH keys\n\n"
+#    print_in_purple "\n • Set up GitHub SSH keys\n\n"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    if ! is_git_repository; then
-        print_error "Not a Git repository"
-        exit 1
-    fi
+#    if ! is_git_repository; then
+#        print_error "Not a Git repository"
+       # exit 1
+#    fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    ssh -T git@github.com &> /dev/null
+#    ssh -T git@github.com &> /dev/null
 
-    if [ $? -ne 1 ]; then
-        set_github_ssh_key
-    fi
+#    if [ $? -ne 1 ]; then
+#        set_github_ssh_key
+#    fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    print_result $? "Set up GitHub SSH keys"
+#    print_result $? "Set up GitHub SSH keys"
 
 }
 
