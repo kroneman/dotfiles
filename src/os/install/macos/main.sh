0b96567df9e345ea36e39a2cb9780a0113679866
Kroneman <a.l.kroneman@gmail.com>
2018-02-16 23:22:49 +0100

    update mac configuration

--- src/os/install/macos/main.sh
+++ src/os/install/macos/main.sh
@@ -6,21 +6,21 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-./xcode.sh
-./homebrew.sh
+#./xcode.sh
+#./homebrew.sh
 ./bash.sh
-./../nvm.sh
+#./../nvm.sh
 
-./browsers.sh
-./compression_tools.sh
-./git.sh
-./gpg.sh
+#./browsers.sh
+#./compression_tools.sh
+#./git.sh
+#./gpg.sh
 ./image_tools.sh
 ./misc.sh
 ./misc_tools.sh
-./../npm.sh
+#./../npm.sh
 ./tmux.sh
-./video_tools.sh
+#./video_tools.sh
 ./../vim.sh
 ./web_font_tools.sh
 
